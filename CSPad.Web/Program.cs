57aa7fea7a17636066b1f66ad034ec400b4d5ca9
André Costa <andreccosta@me.com>
2020-07-11 14:37:26 +0100

    initial version with monaco and compiling done

--- CSPad.Web/Program.cs
+++ CSPad.Web/Program.cs
@@ -18,6 +18,7 @@ namespace CSPad.Web
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<Monaco>();
 
             await builder.Build().RunAsync();
         }
